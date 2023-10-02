4f4c9d6e0b63d638ef3ff60a7232f93fe4f40aec
Ritik Kochar <ritikkochar2@gmail.com>
2023-10-02 21:35:46 +0530

    Add By Default Selected state on all button

--- src/Filter.jsx
+++ src/Filter.jsx
@@ -1,22 +1,34 @@
 import { toast } from "react-toastify";
 
 export default function Filter(props) {
-    let category=props.category;
-    let setCategory=props.setCategory;
+  let category = props.category;
+  let setCategory = props.setCategory;
 
-    function clickHandler(tittle) {
-        toast.dismiss()
-        toast.success("Moved to " + tittle)
-        setCategory(tittle)
-    }
+  function clickHandler(title) {
+    toast.dismiss();
+    toast.success("Moved to " + title);
+    setCategory(title);
+  }
 
-    return(
-        <div className="py-4 text-center">{
-        props.filterData.map((data)=>{   
-            return(
-                <button onClick={()=>clickHandler(data.tittle)} key={data.id} className="focus:border-2 ml-8 px-2 py-1 rounded-md bg-gray-800 text-white">{data.tittle}</button>
-            )
-        })
-    }</div>
-    )
+  return (
+    <div className="py-4 text-center">
+      {props.filterData.map((data) => {
+        const isSelected = data.tittle === category; // Check if the button is selected
+
+        return (
+          <button
+            onClick={() => clickHandler(data.tittle)}
+            key={data.id}
+            className={`ml-8 px-2 py-1 rounded-md ${
+              isSelected
+                ? "bg-blue-500 text-white" 
+                : "bg-gray-800 text-white" 
+            }`}
+          >
+            {data.tittle}
+          </button>
+        );
+      })}
+    </div>
+  );
 }

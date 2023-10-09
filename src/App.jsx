3e4022184a600999a32afd13f863880d32d55719
Nishal Barman <helptechnoapps@gmail.com>
2023-10-09 17:28:13 +0530

    Fixed: #36 | White background issue fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,56 @@
-import React, { useEffect, useState } from 'react';
-import Navbar from './Navbar';
-import Filter from './Filter';
-import {filterData,apirUrl} from './data'
-import Cards from './Cards';
-import Spinner from './Spinner';
-import {toast} from "react-toastify"
+import React, { useEffect, useState } from "react";
+import Navbar from "./Navbar";
+import Filter from "./Filter";
+import { filterData, apirUrl } from "./data";
+import Cards from "./Cards";
+import Spinner from "./Spinner";
+import { toast } from "react-toastify";
+import "./App.css";
 
 function App() {
-  const [courses,setcourses] =useState('');
-  const [Loading,setLoading] =useState(true);
-  const [category,setCategory]=useState(filterData[0].tittle)
+  const [courses, setcourses] = useState("");
+  const [Loading, setLoading] = useState(true);
+  const [category, setCategory] = useState(filterData[0].tittle);
 
   // To fetch the courses and store them in courses array not in objects
   async function fetchData() {
     try {
-    setLoading(true)
-      let response=await fetch(apirUrl);
-      let data= await response.json();
-      setcourses(data.data)
+      setLoading(true);
+      let response = await fetch(apirUrl);
+      let data = await response.json();
+      setcourses(data.data);
     } catch (error) {
-      return(<div>{error}/</div>)
+      return <div>{error}/</div>;
     }
-    setLoading(false)
+    setLoading(false);
   }
 
   // to run in first render
-  useEffect(()=>{
+  useEffect(() => {
     fetchData();
-  },[])
+  }, []);
 
   return (
     <>
       <Navbar />
-      <div className='py-2 bg-gray-800' style={{position: 'sticky',top:0,zIndex:2}}>
-        <Filter filterData={filterData}  category={category} setCategory={setCategory}/>
+      <div
+        className="py-2 bg-gray-800"
+        style={{ position: "sticky", top: 0, zIndex: 2 }}>
+        <Filter
+          filterData={filterData}
+          category={category}
+          setCategory={setCategory}
+        />
       </div>
       <div>
-        {
-          Loading ? (<Spinner/>) : (<Cards courses={courses} category={category}/>) 
-        }
+        {Loading ? (
+          <Spinner />
+        ) : (
+          <Cards courses={courses} category={category} />
+        )}
       </div>
     </>
-  )
+  );
 }
 
 export default App;
--- src/Cards.jsx
+++ src/Cards.jsx
@@ -2,32 +2,34 @@ import { useState } from "react";
 import Card from "./Card";
 import { toast } from "react-toastify";
 
-
-export default function Cards(props) {
-    let category=props.category;
-    let courses=props.courses;
-    const [likedCourses,setLikedCourses]=useState([])
-    function getCourse() {
-        if (category=="All") {
-            let allCourses=[];    
-            Object.values(courses).forEach(array=>{
-                array.forEach(singleCourse=>{
-                    allCourses.push(singleCourse);
-                })
-            })  
-            return allCourses;
-        }
-        else{
-        return courses[category];
-       }
+export default function Cards({ category, courses }) {
+  const [likedCourses, setLikedCourses] = useState([]);
+  
+  function getCourse() {
+    if (category == "All") {
+      let allCourses = [];
+      Object.values(courses).forEach((array) => {
+        array.forEach((singleCourse) => {
+          allCourses.push(singleCourse);
+        });
+      });
+      return allCourses;
+    } else {
+      return courses[category];
     }
-    return(
-        <div className="pt-2 pb-20 flex sm:flex-row flex-col justify-center items-center flex-wrap text-white bg-gray-800 gap-x-3 gap-y-10">
-            {getCourse().map((course)=>{
-                return(
-                    <Card key={course.id} course={course} likedCourses={likedCourses} setLikedCourses={setLikedCourses}/>
-                )
-            })}
-        </div>
-    )
+  }
+  return (
+    <div className="pt-2 pb-20 flex sm:flex-row flex-col justify-center items-center flex-wrap text-white bg-gray-800 gap-x-3 gap-y-10">
+      {getCourse().map((course) => {
+        return (
+          <Card
+            key={course.id}
+            course={course}
+            likedCourses={likedCourses}
+            setLikedCourses={setLikedCourses}
+          />
+        );
+      })}
+    </div>
+  );
 }
